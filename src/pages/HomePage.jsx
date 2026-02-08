f07a7405a4b389545528c48ce94e32e2c2be6783
Amanuel <beginningoflife2324@gmail.com>
2026-02-08 01:54:12 -0900

    feat(services):create section of services with service cards
    Closes #17

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection';
 import AboutMe from '../components/AboutMe';
+import Service from '../components/Service';
 const HomePage = () => {
   return (
   <div  className="home-page m-0 p-0 w-full">
   <HeroSection />
   <AboutMe /> 
+  <Service />
   </div>
    
   )
---
+++ src/stores/Services.js
@@ -0,0 +1,22 @@
+const Services = [
+    {title: "Responsive website development",
+     description:"Mobile-first, fast-loading websites with clean UI.Optimized for all screen sizes and ideal for landing pages, portfolios, and small business websites.",
+        icon: "https://cdn-icons-png.flaticon.com/512/2920/2920277.png"}
+
+    ,
+    { title: "React Web Applications",
+     description: "Build interactive interfaces using ReactComponent-based architecture\nForms, dashboards, and dynamic UI features",
+     icon: "https://cdn-icons-png.flaticon.com/512/919/919851.png"
+    },
+    { title:"UI implementation from Figma designs",
+        description: "Convert Figma designs into pixel-perfect, responsive websites.Clean, maintainable code with close attention to design details.",
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968705.png"
+    },
+    {
+        title: "Bug fixes & frontend improvements",
+        description: "Fix layout issues, responsiveness, and bugs\nImprove UI, performance, and accessibility\nIdeal for existing websites",
+        icon: "https://cdn-icons-png.flaticon.com/512/1828/1828843.png"
+    }
+]
+
+export default Services;
