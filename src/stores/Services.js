711754c108e308b949eebe6b030ae3e20fcf4c83
Amanuel <beginningoflife2324@gmail.com>
2026-02-12 02:30:53 -0900

    chore: update or add message for portfolio

--- src/stores/Services.js
+++ src/stores/Services.js
@@ -1,22 +1,28 @@
 const Services = [
-    {title: "Responsive website development",
-     description:"Mobile-first, fast-loading websites with clean UI.Optimized for all screen sizes and ideal for landing pages, portfolios, and small business websites.",
-        icon: "https://cdn-icons-png.flaticon.com/512/2920/2920277.png"}
-
-    ,
-    { title: "React Web Applications",
-     description: "Build interactive interfaces using ReactComponent-based architecture\nForms, dashboards, and dynamic UI features",
-     icon: "https://cdn-icons-png.flaticon.com/512/919/919851.png"
-    },
-    { title:"UI implementation from Figma designs",
-        description: "Convert Figma designs into pixel-perfect, responsive websites.Clean, maintainable code with close attention to design details.",
-        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968705.png"
-    },
-    {
-        title: "Bug fixes & frontend improvements",
-        description: "Fix layout issues, responsiveness, and bugs\nImprove UI, performance, and accessibility\nIdeal for existing websites",
-        icon: "https://cdn-icons-png.flaticon.com/512/1828/1828843.png"
-    }
-]
+  {
+    title: "Responsive Website Development",
+    description:
+      "Mobile-first, fast-loading websites with clean UI. Optimized for all screen sizes — ideal for landing pages, portfolios, and small business websites.",
+    icon: "https://cdn-icons-png.flaticon.com/512/2920/2920277.png",
+  },
+  {
+    title: "React Web Applications",
+    description:
+      "Interactive, component-based applications using React. Includes forms, Emailjs services,authentication, and dynamic UI features testable With Vitest.",
+    icon: "https://cdn-icons-png.flaticon.com/512/919/919851.png",
+  },
+  {
+    title: "UI Implementation from Figma Designs",
+    description:
+      "Convert Figma designs into pixel-perfect, responsive websites. Clean, maintainable code with close attention to design details and accessibility.",
+    icon: "https://cdn-icons-png.flaticon.com/512/5968/5968705.png",
+  },
+  {
+    title: "Bug Fixes & Frontend Improvements",
+    description:
+      "Resolve layout issues, improve responsiveness, and fix UI bugs. Enhance performance and accessibility for existing websites.",
+    icon: "https://cdn-icons-png.flaticon.com/512/1828/1828843.png",
+  },
+];
 
 export default Services;
